beadba731b66987e709d6a3c997d5b5a6c8dfb31
Justin <justin@invisioncraft.net>
2012-05-17 00:20:02 -0400

    Added config settings

--- net/invisioncraft/plugins/salesmania/configuration/Settings.java
+++ net/invisioncraft/plugins/salesmania/configuration/Settings.java
@@ -2,6 +2,8 @@ package net.invisioncraft.plugins.salesmania.configuration;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.List;
+
 /**
  * Owner: Justin
  * Date: 5/16/12
@@ -15,7 +17,79 @@ public class Settings extends Configuration {
         init();
     }
 
+    // Main
     public String getLocale() {
         return getConfig().getString("Main.locale");
+    }
+
+    public boolean getAllowCreative() {
+        return getConfig().getBoolean("Main.allow creative");
+    }
+
+    // Bidding
+    public int getMinStart() {
+        return getConfig().getInt("Bidding.min start");
+    }
+
+    public int getMaxStart() {
+        return getConfig().getInt("Bidding.max start");
+    }
+
+    public int getMinIncrement() {
+        return getConfig().getInt("Bidding.min increment");
+    }
+
+    public int getMaxIncrement() {
+        return getConfig().getInt("Bidding.max increment");
+    }
+
+    public int getDefaultTime() {
+        return getConfig().getInt("Bidding.default time");
+    }
+
+    public int getSnipeTime() {
+        return getConfig().getInt("Bidding.snipe time");
+    }
+
+    // Logging
+    public boolean isLoggingEnabled() {
+        return getConfig().getBoolean("Logging.enabled");
+    }
+
+    public String getLogFilename() {
+        return getConfig().getString("Logging.filename");
+    }
+
+    public String getLogType() {
+        return getConfig().getString("Logging.type");
+    }
+
+    public String getMysqlHost() {
+        return getConfig().getString("Logging.mysql host");
+    }
+
+    public int getMysqlPort() {
+        return getConfig().getInt("Logging.mysql port");
+    }
+
+    public String getMysqlUsername() {
+        return getConfig().getString("Logging.mysql username");
+    }
+
+    public String getMysqlPassword() {
+        return getConfig().getString("Logging.mysql password");
+    }
+
+    public String getMysqlDatabase() {
+        return getConfig().getString("Logging.mysql database");
+    }
+
+    public String getMysqlTable() {
+        return getConfig().getString("Logging.mysql table");
+    }
+
+    // Blacklist
+    public List<String> getBlacklist() {
+        return getConfig().getStringList("Blacklist");
     }
 }
