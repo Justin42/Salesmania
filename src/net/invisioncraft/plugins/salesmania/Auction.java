c671374f82e52b934b3a25ea2548867d8b3bf6b9
Justin <justin@invisioncraft.net>
2012-05-18 17:57:46 -0400

    Auction API prototyping

--- src/net/invisioncraft/plugins/salesmania/Auction.java
+++ src/net/invisioncraft/plugins/salesmania/Auction.java
@@ -1,9 +1,38 @@
 package net.invisioncraft.plugins.salesmania;
 
+import org.bukkit.entity.Player;
+
 /**
  * Owner: Justin
  * Date: 5/17/12
  * Time: 3:59 PM
  */
 public class Auction {
+    private static Auction instance;
+    private static Salesmania plugin;
+
+    private boolean isRunning = false;
+    private Player winner;
+
+    private long currentBid;
+
+    public static void init(Salesmania plugin) {
+        Auction.plugin = plugin;
+    }
+
+    public void reset() {
+
+    }
+
+    public boolean isRunning() {
+        return isRunning;
+    }
+
+    public Player getWinner() {
+        return winner;
+    }
+
+    public boolean bid(Player player, long bid) {
+        return false;
+    }
 }
