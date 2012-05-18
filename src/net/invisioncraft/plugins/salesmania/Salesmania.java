5e043dbd8c4f368719e7160ba62715645b04654d
Justin <justin@invisioncraft.net>
2012-05-18 17:58:21 -0400

    Changed some variables to public

--- src/net/invisioncraft/plugins/salesmania/Salesmania.java
+++ src/net/invisioncraft/plugins/salesmania/Salesmania.java
@@ -8,14 +8,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.logging.Logger;
 
 public class Salesmania extends JavaPlugin {
-    Logger consoleLogger;
-    Economy economy;
-    Settings settings;
+    public Logger consoleLogger;
+    public Economy economy;
+    public Settings settings;
 
     @Override
     public void onEnable() {
         settings = new Settings(this);
-
         consoleLogger = this.getLogger();
 
         getCommand("auction").setExecutor(new AuctionCommandExecutor(this));
