abbaf3226a1baa485979c6bb00268dce6101d116
Justin <justin@invisioncraft.net>
2012-05-20 09:16:13 -0400

    Added getAuction method

--- src/net/invisioncraft/plugins/salesmania/Salesmania.java
+++ src/net/invisioncraft/plugins/salesmania/Salesmania.java
@@ -1,6 +1,7 @@
 package net.invisioncraft.plugins.salesmania;
 
 import net.invisioncraft.plugins.salesmania.commands.auction.AuctionCommandExecutor;
+import net.invisioncraft.plugins.salesmania.configuration.Locale;
 import net.invisioncraft.plugins.salesmania.configuration.Settings;
 import net.milkbowl.vault.economy.Economy;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -11,11 +12,12 @@ public class Salesmania extends JavaPlugin {
     public Logger consoleLogger;
     public Economy economy;
     public Settings settings;
-
+    public Auction currentAuction;
     @Override
     public void onEnable() {
         settings = new Settings(this);
         consoleLogger = this.getLogger();
+        Locale.init(this, settings.getLocale());
 
         getCommand("auction").setExecutor(new AuctionCommandExecutor(this));
 
@@ -29,5 +31,12 @@ public class Salesmania extends JavaPlugin {
 
     public Settings getSettings() {
         return settings;
+    }
+
+    public Auction getAuction() {
+        if(currentAuction == null) {
+            currentAuction = new Auction(this);
+        }
+        return currentAuction;
     }
 }
