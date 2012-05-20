53b88cee414e41185d32e2e812d77c266f63e26d
Justin <justin@invisioncraft.net>
2012-05-20 07:59:12 -0400

    Minor

--- src/net/invisioncraft/plugins/salesmania/configuration/Locale.java
+++ src/net/invisioncraft/plugins/salesmania/configuration/Locale.java
@@ -1,24 +1,22 @@
 package net.invisioncraft.plugins.salesmania.configuration;
 
 import net.invisioncraft.plugins.salesmania.Salesmania;
-import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * Owner: Justin
  * Date: 5/16/12
  * Time: 7:29 PM
  */
-public class Locale extends Configuration {
+public class Locale  {
     Salesmania salesMania;
-
     private static Configuration config;
 
     public static void init(Salesmania plugin, String locale) {
-        Locale.config = new Configuration(plugin, locale + ".yml");
+        config = new Configuration(plugin, locale + ".yml");
     }
 
-    public static void getMessage(String path) {
-
+    public static String getMessage(String path) {
+        return config.getConfig().getString(path);
     }
 
 
