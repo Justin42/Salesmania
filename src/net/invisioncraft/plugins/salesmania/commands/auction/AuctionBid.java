5ddf69b693dbad526ca2a4df0be19d793e92785b
Justin <justin@invisioncraft.net>
2012-05-20 09:17:52 -0400

    Started implementing the AuctionBid command handler

--- src/net/invisioncraft/plugins/salesmania/commands/auction/AuctionBid.java
+++ src/net/invisioncraft/plugins/salesmania/commands/auction/AuctionBid.java
@@ -1,8 +1,10 @@
 package net.invisioncraft.plugins.salesmania.commands.auction;
 
+import net.invisioncraft.plugins.salesmania.Salesmania;
+import net.invisioncraft.plugins.salesmania.configuration.Locale;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.entity.Player;
 
 /**
  * Owner: Justin
@@ -11,12 +13,24 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class AuctionBid extends AuctionCommand {
 
-    public AuctionBid(JavaPlugin plugin) {
+    public AuctionBid(Salesmania plugin) {
         super(plugin);
     }
 
     @Override
     public boolean execute(CommandSender sender, Command command, String label, String[] args) {
-        return false;  //To change body of implemented methods use File | Settings | File Templates.
+        if(!(sender instanceof Player)) {
+            sender.sendMessage(Locale.getMessage("Bidding.consoleCantBid"));
+        }
+        Player player = (Player) sender;
+        long bidAmount = Long.valueOf(args[0]);
+
+        switch(plugin.getAuction().bid(player, bidAmount)) {
+            case SUCCESS:
+                player.sendMessage(Locale.getMessage("Bidding.budSuccess"));
+                return true;
+        }
+
+        return false;
     }
 }
