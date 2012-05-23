e32cfcef5494b65e6a4c6d1d44af2cd2146a01ba
Justin <justin@invisioncraft.net>
2012-05-23 06:22:15 -0400

    Implemented AuctionStartEvent

---
+++ src/net/invisioncraft/plugins/salesmania/event/AuctionStartEvent.java
@@ -0,0 +1,44 @@
+package net.invisioncraft.plugins.salesmania.event;
+
+import net.invisioncraft.plugins.salesmania.Auction;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.bukkit.inventory.ItemStack;
+
+/**
+ * Owner: Justin
+ * Date: 5/23/12
+ * Time: 4:42 AM
+ */
+public class AuctionStartEvent extends Event {
+    private static final HandlerList handlers = new HandlerList();
+    Player owner;
+    ItemStack itemStack;
+    long startingBid;
+    public AuctionStartEvent(Auction auction) {
+        owner = auction.getOwner();
+        itemStack = auction.getItemStack();
+        startingBid = auction.getCurrentBid();
+    }
+
+    public Player getOwner() {
+        return owner;
+    }
+
+    public ItemStack getItemStack() {
+        return itemStack;
+    }
+
+    public long getStartingBid() {
+        return startingBid;
+    }
+
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
