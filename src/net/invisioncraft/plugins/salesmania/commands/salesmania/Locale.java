4fc1212620f819cd89b97c14ec79afb8e3408c57
Justin <justin@invisioncraft.net>
2012-05-23 20:23:39 -0400

    Created Locale command handler and SalesmaniaCommandExecutor

---
+++ src/net/invisioncraft/plugins/salesmania/commands/salesmania/Locale.java
@@ -0,0 +1,23 @@
+package net.invisioncraft.plugins.salesmania.commands.salesmania;
+
+import net.invisioncraft.plugins.salesmania.CommandHandler;
+import net.invisioncraft.plugins.salesmania.Salesmania;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+/**
+ * Owner: Justin
+ * Date: 5/23/12
+ * Time: 8:01 PM
+ */
+public class Locale extends CommandHandler {
+
+    public Locale(Salesmania plugin) {
+        super(plugin);
+    }
+
+    @Override
+    public boolean execute(CommandSender sender, Command command, String label, String[] args) {
+        return false;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
---
+++ src/net/invisioncraft/plugins/salesmania/commands/salesmania/SalesmaniaCommandExecutor.java
@@ -0,0 +1,18 @@
+package net.invisioncraft.plugins.salesmania.commands.salesmania;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+/**
+ * Owner: Justin
+ * Date: 5/23/12
+ * Time: 8:08 PM
+ */
+public class SalesmaniaCommandExecutor extends CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        return false;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
