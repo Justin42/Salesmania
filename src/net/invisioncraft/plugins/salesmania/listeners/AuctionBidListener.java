e78dcaf59959dfd787ebb280798742d511e8eabf
Justin <justin@invisioncraft.net>
2012-05-25 01:24:37 -0400

    Created Auction event listeners

---
+++ src/net/invisioncraft/plugins/salesmania/listeners/AuctionBidListener.java
@@ -0,0 +1,9 @@
+package net.invisioncraft.plugins.salesmania.listeners;
+
+/**
+ * Owner: Justin
+ * Date: 5/25/12
+ * Time: 1:24 AM
+ */
+public class AuctionBidListener {
+}
---
+++ src/net/invisioncraft/plugins/salesmania/listeners/AuctionTimerListener.java
@@ -0,0 +1,9 @@
+package net.invisioncraft.plugins.salesmania.listeners;
+
+/**
+ * Owner: Justin
+ * Date: 5/25/12
+ * Time: 1:23 AM
+ */
+public class AuctionTimerListener {
+}
