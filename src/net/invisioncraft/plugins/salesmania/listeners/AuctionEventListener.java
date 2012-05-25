fcd3fb628940998da16e3672bcef50f7ce667761
Justin <justin@invisioncraft.net>
2012-05-25 05:21:01 -0400

    Began implementing the new AuctionEvent handler

--- src/net/invisioncraft/plugins/salesmania/listeners/AuctionEventListener.java
+++ src/net/invisioncraft/plugins/salesmania/listeners/AuctionEventListener.java
@@ -1,9 +1,26 @@
 package net.invisioncraft.plugins.salesmania.listeners;
 
+import net.invisioncraft.plugins.salesmania.event.AuctionEvent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
 /**
  * Owner: Justin
  * Date: 5/25/12
  * Time: 5:08 AM
  */
-public class AuctionEventListener {
+public class AuctionEventListener implements Listener {
+    @EventHandler
+    public void onAuctionEvent(AuctionEvent auctionEvent) {
+        switch (auctionEvent.getEventType()) {
+            case BID:
+                break;
+            case END:
+                break;
+            case START:
+                break;
+            case TIMER:
+                break;
+        }
+    }
 }
