a176079744e64639f3cf9554ce5d96504fde48e6
Justin <justin@invisioncraft.net>
2012-05-28 16:36:28 -0400

    Added a settings variable for CommandHandler

--- src/net/invisioncraft/plugins/salesmania/CommandHandler.java
+++ src/net/invisioncraft/plugins/salesmania/CommandHandler.java
@@ -1,6 +1,7 @@
 package net.invisioncraft.plugins.salesmania;
 
 import net.invisioncraft.plugins.salesmania.configuration.LocaleHandler;
+import net.invisioncraft.plugins.salesmania.configuration.Settings;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
@@ -12,10 +13,12 @@ import org.bukkit.command.CommandSender;
 public abstract class CommandHandler {
     protected Salesmania plugin;
     protected LocaleHandler localeHandler;
+    protected Settings settings;
 
     public CommandHandler(Salesmania plugin) {
         this.plugin = plugin;
         localeHandler = plugin.getLocaleHandler();
+        settings = plugin.getSettings();
     }
 
     public abstract boolean execute(CommandSender sender, Command command, String label, String[] args);
