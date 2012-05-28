b65806be31b2ee6d7b78247058f06985a5b2cd34
Justin <justin@invisioncraft.net>
2012-05-28 16:39:47 -0400

    Made Locale extend Configuration

--- src/net/invisioncraft/plugins/salesmania/configuration/Locale.java
+++ src/net/invisioncraft/plugins/salesmania/configuration/Locale.java
@@ -1,7 +1,6 @@
 package net.invisioncraft.plugins.salesmania.configuration;
 
 import net.invisioncraft.plugins.salesmania.Salesmania;
-import org.bukkit.configuration.file.FileConfiguration;
 
 import java.util.List;
 
@@ -10,21 +9,21 @@ import java.util.List;
  * Date: 5/16/12
  * Time: 7:29 PM
  */
-public class Locale  {
-    Salesmania salesMania;
-    private static FileConfiguration config;
+public class Locale extends Configuration {
     private String localeName;
     public Locale(Salesmania plugin, String locale) {
+        super(plugin, locale + ".yml");
         localeName = locale;
-        config = new Configuration(plugin, locale + ".yml").getConfig();
+        plugin.getLogger().info(String.format("Loaded locale messages for %s", locale));
     }
 
     public String getMessage(String path) {
-        return config.getString(path);
+        if(getConfig().contains(path)) return getConfig().getString(path);
+        else return "Locale message not found.";
     }
 
     public List<String> getMessageList(String path) {
-        return config.getStringList(path);
+        return getConfig().getStringList(path);
     }
 
     public String getName() {
