adc755a639213500ffc21ce216a12a51438d926b
Justin <justin@invisioncraft.net>
2012-06-02 03:00:05 -0400

    Added licensing

--- src/net/invisioncraft/plugins/salesmania/event/AuctionEvent.java
+++ src/net/invisioncraft/plugins/salesmania/event/AuctionEvent.java
@@ -9,6 +9,21 @@ import org.bukkit.event.HandlerList;
  * Date: 5/25/12
  * Time: 4:46 AM
  */
+/*
+Copyright 2012 Byte 2 O Software LLC
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 public class AuctionEvent extends Event {
     private static final HandlerList handlers = new HandlerList();
 
