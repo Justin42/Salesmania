e9c043dc54da55166ee38d466b9114bb47ceb769
Justin <justin@invisioncraft.net>
2012-06-04 19:34:54 -0400

    implemented ItemManager.takeItem
    added data and enchantment checks for getQuantity

--- src/net/invisioncraft/plugins/salesmania/util/ItemManager.java
+++ src/net/invisioncraft/plugins/salesmania/util/ItemManager.java
@@ -5,6 +5,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.Map;
+import java.util.logging.Logger;
 
 /**
  * Owner: Justin
@@ -13,20 +14,47 @@ import java.util.Map;
  */
 public class ItemManager {
     private Salesmania plugin;
-
+    private static Logger consoleLogger = Salesmania.consoleLogger;
     public ItemManager(Salesmania plugin) {
         this.plugin = plugin;
     }
 
-    public static int getQuantity(Player player, int itemID) {
+    public static int getQuantity(Player player, ItemStack itemStack) {
         int quantity = 0;
-        for(Map.Entry<Integer, ? extends ItemStack> entry : player.getInventory().all(itemID).entrySet()) {
+        for(Map.Entry<Integer, ? extends ItemStack> entry : player.getInventory().all(itemStack.getTypeId()).entrySet()) {
+            // Check for data value + enchants
+            if(!entry.getValue().getEnchantments().equals(itemStack.getEnchantments())) continue;
+            if(!entry.getValue().getData().equals(itemStack.getData())) continue;
             quantity += entry.getValue().getAmount();
         }
         return quantity;
     }
 
-    public static int getQuantity(Player player, ItemStack itemStack) {
-        return getQuantity(player, itemStack.getTypeId());
+    public static boolean takeItem(Player player, ItemStack itemStack) {
+        int remainingQuantity = itemStack.getAmount();
+        for(Map.Entry<Integer, ? extends ItemStack> entry : player.getInventory().all(itemStack.getTypeId()).entrySet()) {
+
+            // Check for data value + enchants
+            if(!entry.getValue().getEnchantments().equals(itemStack.getEnchantments())) continue;
+            if(!entry.getValue().getData().equals(itemStack.getData())) continue;
+
+            if(remainingQuantity == 0) break;
+            ItemStack stack = entry.getValue();
+
+            if(remainingQuantity >= stack.getAmount()) {
+                remainingQuantity -= stack.getAmount();
+                player.getInventory().removeItem(stack);
+            }
+
+            else if (remainingQuantity < stack.getAmount()) {
+                stack.setAmount(stack.getAmount()-remainingQuantity);
+                remainingQuantity -= remainingQuantity;
+            }
+        }
+        if(remainingQuantity != 0) {
+            consoleLogger.severe("Could not take expected quantity!");
+            return false;
+        }
+        else return true;
     }
 }
